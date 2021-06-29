a3a2491558706fc53cf951fe0522244f41f75c90
manig1729 <manikandang1729@gmail.com>
2021-06-30 00:53:53 +0530

    add a -y in i3-install.sh so the installation doesn't wait for user input in between

--- scripts/i3-install.sh
+++ scripts/i3-install.sh
@@ -6,7 +6,7 @@ sudo apt install -y i3
 # i3 gaps
 
 # Dependencies
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
+sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
 
 sudo add-apt-repository ppa:kgilmer/speed-ricer
 sudo apt-get update
