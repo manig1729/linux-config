dbf71a634e6ec4f5757ba36e68e720d2eecfbd79
manig1729 <manikandang1729@gmail.com>
2021-06-29 19:52:03 +0530

    Updated setup.sh to make dotfile.sh work correctly. It has to be run from the linux-config directory

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,10 +2,10 @@
 
 # Master setup file
 
-./apt-install.sh
-./i3-install.sh
-./youtube-dl-install.sh
-../dotfiles.sh
+./scripts/apt-install.sh
+./scripts/i3-install.sh
+./scripts/youtube-dl-install.sh
+./dotfiles.sh
 
 # Get all upgrades
 sudo apt upgrade -y
